64e34a7cb6a307ba6673bb3e85cbac00c5c86026
Lukas Schoerghuber <lukas.schoerghuber@posteo.at>
2024-10-01 18:36:43 +0200

    Create a web application

--- index.ts
+++ index.ts
@@ -1,47 +1,48 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 import { RisJourneyType } from "./dto/RisJourneyType";
 import type { RisTripRequestLeg } from "./dto/RisTripRequest";
 import { TravellerPersona } from "./dto/TravellerPersona";
 import { queryRouting } from "./ris/RisRoutingAccessor";
 import { createTrip } from "./ris/RisTripAccessor";
 
-// const risRoutingResponse = await queryRouting("8011160", "8102067", "2024-10-01T06:26:00Z");
-const risRoutingResponse = await queryRouting("8011160", "8001825", "2024-10-01T06:26:00Z");
-
 const SERVER_PORT = 8080;
 
-const webApplication = new Elysia();
-webApplication.post("/registerJourney", async () => {
-    const trip = risRoutingResponse.trips[0];
-    const legs = trip.legs.filter((leg) => leg.type === RisJourneyType.JOURNEY);
-    const tripLegs = legs.map((leg) => {
-        const tripLeg: RisTripRequestLeg = {
-            type: leg.type as "JOURNEY",
-            departure: {
-                evaNumber: leg.departure.stopPlace.evaNumber,
-                date: leg.departure.timeSchedule.substring(0, 10),
-                timeSchedule: leg.departure.timeSchedule,
-                category: leg.departure.transport.category,
-                journeyNumber: leg.departure.transport.number,
-            },
-            arrival: {
-                evaNumber: leg.arrival.stopPlace.evaNumber,
-                date: leg.arrival.timeSchedule.substring(0, 10),
-                timeSchedule: leg.arrival.timeSchedule,
-                category: leg.arrival.transport.category,
-                journeyNumber: leg.arrival.transport.number,
-            },
+// curl -X POST -i http://127.0.0.1:8080/registerJourney/8011160/8102067/2024-10-01T06:26:00Z
+const webApplication = new Elysia()
+    .use(swagger())
+    .post("/registerJourney/:departureId/:arrivalId/:departureDate", async ({ params: { departureId, arrivalId, departureDate } }) => {
+        const risRoutingResponse = await queryRouting(departureId, arrivalId, departureDate);
+
+        const trip = risRoutingResponse.trips[0];
+        const legs = trip.legs.filter((leg) => leg.type === RisJourneyType.JOURNEY);
+        const tripLegs = legs.map((leg) => {
+            const tripLeg: RisTripRequestLeg = {
+                type: leg.type as "JOURNEY",
+                departure: {
+                    evaNumber: leg.departure.stopPlace.evaNumber,
+                    date: leg.departure.timeSchedule.substring(0, 10),
+                    timeSchedule: leg.departure.timeSchedule,
+                    category: leg.departure.transport.category,
+                    journeyNumber: leg.departure.transport.number,
+                },
+                arrival: {
+                    evaNumber: leg.arrival.stopPlace.evaNumber,
+                    date: leg.arrival.timeSchedule.substring(0, 10),
+                    timeSchedule: leg.arrival.timeSchedule,
+                    category: leg.arrival.transport.category,
+                    journeyNumber: leg.arrival.transport.number,
+                },
+            };
+            return tripLeg;
+        });
+        console.log(tripLegs);
+        const tripId = await createTrip(
+            TravellerPersona.OCCASIONAL_TRAVELLER,
+            tripLegs
+        );
+        return {
+            "tripID": tripId,
         };
-        return tripLeg;
-    });
-    console.log(tripLegs);
-    const tripId = await createTrip(
-        TravellerPersona.OCCASIONAL_TRAVELLER,
-        tripLegs
-    );
-    return {
-        "tripID": tripId,
-    };
-});
-webApplication.listen(SERVER_PORT);
+    }).listen(SERVER_PORT);
 console.log("web application listening at port " + SERVER_PORT);
